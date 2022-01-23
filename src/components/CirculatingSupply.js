7fd99b84fac573ef55688bf7a7bef67ca5df6f5e
0xKoji <92653490+0xKoji@users.noreply.github.com>
2022-01-23 03:59:02 +0000

    Fix

--- src/components/CirculatingSupply.js
+++ src/components/CirculatingSupply.js
@@ -7,10 +7,12 @@ const CirculatingSupply = () => {
     const [supply, setSupply] = useState(0)
 
 
-    useEffect(async() => {
-        const gecko = await (await fetch('https://api.coingecko.com/api/v3/coins/vader-protocol')).json()
+    useEffect(() => {
+        (async() => {
+            const gecko = await (await fetch('https://api.coingecko.com/api/v3/coins/vader-protocol')).json()
         
-        setSupply(gecko.market_data.circulating_supply)
+            setSupply(gecko.market_data.circulating_supply)
+        })()
     }, [])
 
     return <Card title='Circulating Supply' desc={Number(supply).toLocaleString()} />
